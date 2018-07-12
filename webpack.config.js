5163cf59c9a18812f6ba1db91f57876f534028ad
linlei <512102250@qq.com>
2018-07-12 08:43:11 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+    entry:{
+        index:'./src/js/index.js',
+    },
+    output:{
+        path:__dirname,
+        filename:'bundle.js',
+        publicPath:'./img'
+    },
+    // devServer:{
+    //     // historyApiFallback:true,
+    //     hot:true,
+    //     inline:true,
+    //     progress:true,
+    //     port:9090, //端口你可以自定义
+    // },
+    module:{
+        rules:[
+            {test:/.less$/,use:['style-loader','css-loader','less-loader']},
+            {test:/.jpg|.png$/,use:['url-loader?limit=60&name=/[name].[ext]']},
+        ]
+    },
+    mode:'development',
+}
